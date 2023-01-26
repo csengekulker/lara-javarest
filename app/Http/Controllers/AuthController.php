bb9f5f3d294faf23ec7d81eda8d1c4b532925cc0
csengekulker <k71608883429@ktch.hu>
2023-01-26 11:10:35 +0100

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
-    //
+    public function signup( Request $request ) {
+
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            "name" => "required",
+            "email" => "required",
+            "password" => "required",
+            "confirm_password" => "required|same:password"
+        ]);
+
+        if($validator->fails()) {
+            return $this->sendError("signup fail", $validator->errors());
+        }
+
+        $input["password"] = bcrypt($input["password"]);
+        $user = User::create($input);
+
+        return $this->sendResponse( $user, "signup OK");
+    }
+
+    public function login( Request $request ) {
+
+        if(Auth::attempt([ 
+            "email" => $request->email, 
+            "password" => $request->password 
+        ])) {
+            $authUser = Auth::user();
+            $success["token"] = 
+                $authUser->createToken("myauthapp")->plainTextToken;
+            $success["name"] = $authUser->name;
+
+            return $this->sendResponse( $success, "hello");
+        } else {
+            return $this->sendError("login fail", [ "error" => "sikertelen"]);
+        }
+    }
+
+    public function logout (Request $request) {
+
+        auth("sanctum")->user()->currentAccessToken()->delete();
+
+        return response()->json("logout OK");
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("signup", [AuthController::class, 'signup']);
+Route::post("login", [AuthController::class, 'login']);
+Route::post("logout", [AuthController::class, 'logout']);
+
+//vedett
 Route::get("products", [ProductController::class, 'index']);
 Route::post("new-product", [ProductController::class, 'create']);
 Route::get("products/{id}", [ProductController::class, 'show']);
